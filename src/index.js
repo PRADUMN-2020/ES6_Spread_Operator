cb320dabbcf4e34206d9e893fed0aa1a6a257b1a
Pradumn Prasad <PRADUMNPRASAD883@GMAIL.COM>
2023-04-07 23:38:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
+
+const fullName = {
+  fName: "",
+  lName: "Bond",
+  fName: "hello"
+};
+
+console.log(fullName);
+
+// const user = {
+//   fullName,
+//   id: 1,
+//   username: "jamesbond007"
+// };
+
+// console.log(user);
